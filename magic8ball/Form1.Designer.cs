aa2892aaaec1edf2c70899e62364bc125a347e9f
Ricko0702 <rickvanassen2@gmail.com>
2022-11-16 20:05:41 +0100

    Add project files.

---
+++ magic8ball/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace magic8ball
+{
+    partial class MagicEightBall
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.submitButton = new System.Windows.Forms.Button();
+            this.questionBox = new System.Windows.Forms.TextBox();
+            this.answerLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // submitButton
+            // 
+            this.submitButton.BackColor = System.Drawing.SystemColors.MenuText;
+            this.submitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitButton.ForeColor = System.Drawing.Color.White;
+            this.submitButton.Location = new System.Drawing.Point(12, 388);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(290, 50);
+            this.submitButton.TabIndex = 0;
+            this.submitButton.Text = "Tell me!";
+            this.submitButton.UseVisualStyleBackColor = false;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // questionBox
+            // 
+            this.questionBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.questionBox.Location = new System.Drawing.Point(12, 342);
+            this.questionBox.Multiline = true;
+            this.questionBox.Name = "questionBox";
+            this.questionBox.Size = new System.Drawing.Size(290, 40);
+            this.questionBox.TabIndex = 1;
+            this.questionBox.Text = "Ask your question here...";
+            this.questionBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // answerLabel
+            // 
+            this.answerLabel.BackColor = System.Drawing.Color.Transparent;
+            this.answerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.answerLabel.ForeColor = System.Drawing.Color.Black;
+            this.answerLabel.Location = new System.Drawing.Point(355, 138);
+            this.answerLabel.Name = "answerLabel";
+            this.answerLabel.Size = new System.Drawing.Size(118, 47);
+            this.answerLabel.TabIndex = 2;
+            this.answerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.answerLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // MagicEightBall
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.answerLabel);
+            this.Controls.Add(this.questionBox);
+            this.Controls.Add(this.submitButton);
+            this.Name = "MagicEightBall";
+            this.Text = "Magic 8 ball";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.TextBox questionBox;
+        private System.Windows.Forms.Label answerLabel;
+    }
+}
+
